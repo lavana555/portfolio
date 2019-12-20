313b04f5360ee449817a88344545e9bee3ff368e
lavana555 <timofeevniki3@gmail.com>
2019-12-20 18:52:07 +0300

    Updates

---
+++ precache-manifest.fe12c7129a061386f01a23d7ed982245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25c5b6a06e527aaa4c5bbeee262e56e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b8fdc78a98a5559be34",
+    "url": "/portfolio/static/css/main.22de60cf.chunk.css"
+  },
+  {
+    "revision": "c2020ccf5201aa434f0c",
+    "url": "/portfolio/static/js/2.a21ee742.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.a21ee742.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b8fdc78a98a5559be34",
+    "url": "/portfolio/static/js/main.7a91cde1.chunk.js"
+  },
+  {
+    "revision": "8e33e263dd9f3af2ec21",
+    "url": "/portfolio/static/js/runtime-main.c6ae4aa1.js"
+  }
+]);
