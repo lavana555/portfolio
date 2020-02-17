1730b3039d5ccc8f65a47180fcea16ff0b0219f9
lavana555 <timofeevniki3@gmail.com>
2020-02-17 15:21:34 +0300

    Updates

---
+++ precache-manifest.5bb5a2b627c7397501c85e73b601d65a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b616b37d114bc4a1b7d90874c90f614b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a3741a07f58a025873c",
+    "url": "/portfolio/static/css/main.707add42.chunk.css"
+  },
+  {
+    "revision": "ed025820fa45678ddfec",
+    "url": "/portfolio/static/js/2.881ebe3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.881ebe3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a3741a07f58a025873c",
+    "url": "/portfolio/static/js/main.485f8828.chunk.js"
+  },
+  {
+    "revision": "8e33e263dd9f3af2ec21",
+    "url": "/portfolio/static/js/runtime-main.c6ae4aa1.js"
+  },
+  {
+    "revision": "73004290bd51f12c2a79b17722a2d9bc",
+    "url": "/portfolio/static/media/css-3.73004290.svg"
+  },
+  {
+    "revision": "91aa098d0a4526374b13a7197acb2a60",
+    "url": "/portfolio/static/media/dogs.91aa098d.png"
+  },
+  {
+    "revision": "809ea75e1f956a570fbdfa577888a469",
+    "url": "/portfolio/static/media/faceboo.809ea75e.svg"
+  },
+  {
+    "revision": "21d620ef0ececdc58f216f04f20d915b",
+    "url": "/portfolio/static/media/github.21d620ef.svg"
+  },
+  {
+    "revision": "4b7156791ee09be5d834d28fe017ec10",
+    "url": "/portfolio/static/media/html-5.4b715679.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/portfolio/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "8d317898685887c3eb11ab64f27b6783",
+    "url": "/portfolio/static/media/linkedin.8d317898.svg"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/portfolio/static/media/react.eab76719.svg"
+  },
+  {
+    "revision": "de5a9bfb144145dee24d3b30586f6980",
+    "url": "/portfolio/static/media/schetchik.de5a9bfb.png"
+  },
+  {
+    "revision": "0554b06908adee6eb05201bb040307bc",
+    "url": "/portfolio/static/media/telegram.0554b069.svg"
+  },
+  {
+    "revision": "4b7f4666cbefe31b0a1c3e7ffd04ebec",
+    "url": "/portfolio/static/media/todolist.4b7f4666.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d36515d6e8fbb83aac3f4381cfd39f89.js"
+  "/portfolio/precache-manifest.5bb5a2b627c7397501c85e73b601d65a.js"
 );
 
 self.addEventListener('message', (event) => {
